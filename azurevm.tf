d3138b9a6fe642ac69b02eb06bcf36b4c4362c4c
narayudu <narayudu21@gmail.com>
2021-07-14 15:59:33 +0530

    Create azurevm.tf

---
+++ azurevm.tf
@@ -0,0 +1,115 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "registry.terraform.io/hashicorp/azurerm"
+      version = "2.44.0"
+    }
+  }
+}
+
+provider "azurerm" {
+subscription_id = "5d9b8468-fded-4c10-8e52-b16b4c4549f3"
+client_id = "290b8c87-04fe-49c1-a51c-e061b12e39b8"
+client_secret = "BE~-PY5idF16VYJ6__bqZF2TG53uxDOHRw"
+tenant_id = "71317406-1f5e-4cb7-b88b-3f17530e8758"
+features {}
+}
+
+resource "azurerm_resource_group" "ubuntu" {
+  name     = "ubuntu-resources"
+  location = "West US 3"
+}
+
+resource "azurerm_virtual_network" "ubuntu" {
+  name                = "ubuntu-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.ubuntu.location
+  resource_group_name = azurerm_resource_group.ubuntu.name
+}
+
+resource "azurerm_subnet" "ubuntu" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.ubuntu.name
+  virtual_network_name = azurerm_virtual_network.ubuntu.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "ubuntu" {
+  name                = "ubuntu-nic"
+  location            = azurerm_resource_group.ubuntu.location
+  resource_group_name = azurerm_resource_group.ubuntu.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.ubuntu.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ubuntu.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "ubuntu" {
+  name                = "ubuntu-machine"
+  resource_group_name = azurerm_resource_group.ubuntu.name
+  location            = azurerm_resource_group.ubuntu.location
+  size                = "Standard_ds1_v2"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.ubuntu.id,
+  ]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "19.04"
+    version   = "latest"
+  }
+
+
+computer_name = "ubuntu-machine"
+admin_username = "adminuser"
+admin_password = "Sapubuntu@123"
+}
+
+resource "azurerm_public_ip" "ubuntu" {
+  name                = "ubuntu0001publicip1"
+  resource_group_name = azurerm_resource_group.ubuntu.name
+  location            = azurerm_resource_group.ubuntu.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_security_group" "ubuntu" {
+  name                = "ubuntu-security-group1"
+  location            = azurerm_resource_group.ubuntu.location
+  resource_group_name = azurerm_resource_group.ubuntu.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+resource "azurerm_network_interface_security_group_association" "ubuntu" {
+    network_interface_id      = azurerm_network_interface.ubuntu.id
+    network_security_group_id = azurerm_network_security_group.ubuntu.id
+}
+© 2021 GitHub, Inc.
