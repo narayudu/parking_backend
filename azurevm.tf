f39468046b9cba5fb37f4a3b98d3af1986775026
narayudu <narayudu21@gmail.com>
2021-07-18 18:08:12 +0530

    Update azurevm.tf

--- azurevm.tf
+++ azurevm.tf
@@ -66,7 +66,7 @@ resource "azurerm_linux_virtual_machine" "ubuntu" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "19.04"
+    sku       = "18.04 LTS"
     version   = "latest"
   }
 
